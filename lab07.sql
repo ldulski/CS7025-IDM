aee386ac2dd3b53bae7aa9188a255ab487e3f5b5
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2025-11-20 10:41:00 +0000

    Starting lab 07

---
+++ lab07.sql
@@ -0,0 +1,32 @@
+CREATE TABLE dantes_inferno (
+    column_circle TINYTEXT,
+    column_punishment LONGTEXT,
+);
+
+INSERT INTO
+    dantes_inferno (column_circle)
+VALUES (
+        "Limbo",
+        "Lust",
+        "Gluttony",
+        "Greed",
+        "Wrath",
+        "Heresy",
+        "Violence",
+        "Fruad",
+        "Treachery"
+    );
+
+INSERT INTO
+    dantes_inferno (column_circle)
+VALUES (
+        "Being devoid of full presence and joy",
+        "Being swept through the air by hot winds",
+        "Being drenched in freezing rain and eating putrid slime",
+        "Forced to push massive boulders",
+        "Forced to engage in an eternal battle",
+        "Being confined to a tomb",
+        "Swim in a lake of fire and blood",
+        "Forced to engage in an eternal battle",
+        "Your soul is froze far from God's light"
+    )

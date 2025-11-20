045157dd624929862bfeb9ba56f5da847fc900ca
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2025-11-20 10:56:23 +0000

    Edits

--- lab07.sql
+++ lab07.sql
@@ -5,10 +5,6 @@ CREATE TABLE dantes_inferno (
 );
 
 INSERT INTO
-    dantes_inferno (column_ring)
-VALUES (1, 2, 3, 4, 5, 6, 7, 8, 9,);
-
-INSERT INTO
     dantes_inferno (column_circle)
 VALUES (
         "Limbo",

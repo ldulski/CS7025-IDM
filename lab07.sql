c9d9a073860a52d71c144e4791347bfecdf9e5d0
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2025-11-20 11:30:46 +0000

    Editing SQL

--- lab07.sql
+++ lab07.sql
@@ -1,33 +1,32 @@
 CREATE TABLE dantes_inferno (
-    column_ring INTEGER,
-    column_circle TINYTEXT,
-    column_punishment LONGTEXT,
+    id INTEGER,
+    ring TEXT,
+    punishment TEXT,
 );
 
-INSERT INTO
-    dantes_inferno (column_circle)
-VALUES (
-        "Limbo",
-        "Lust",
-        "Gluttony",
-        "Greed",
-        "Wrath",
-        "Heresy",
-        "Violence",
-        "Fruad",
-        "Treachery"
-    );
+INSERT INTO dantes_inferno (id, ring, punishment)
+(1,"Limbo", "Being devoid of full presence and joy");
 
-INSERT INTO
-    dantes_inferno (column_circle)
-VALUES (
-        "Being devoid of full presence and joy",
-        "Being swept through the air by hot winds",
-        "Being drenched in freezing rain and eating putrid slime",
-        "Forced to push massive boulders",
-        "Forced to engage in an eternal battle",
-        "Being confined to a tomb",
-        "Swim in a lake of fire and blood",
-        "Forced to engage in an eternal battle",
-        "Your soul is froze far from God's light"
-    );
+INSERT INTO dantes_inferno (id, ring, punishment)
+(2,"Lust", "Being swept through the air by hot winds");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(3,"Gluttony", "Being drenched in freezing rain and eating putrid slime");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(4,"Greed", "Forced to push a boulder eternally");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(5,"Wrath", "Forced to engage in an eternal battle");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(6,"Heresy", "Swim in a lake of fire and blood");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(7,"Violence", "3 concentric rings with different punishments");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(8,"Fruad", "A malebolge with different punishments");
+
+INSERT INTO dantes_inferno (id, ring, punishment)
+(9, "Treachery", "Your soul is froze far from God's light");
